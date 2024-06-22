0f15294d3a9f9a44913c06c73dd92c23a661aaf4
SerendipitySynapses <hosseinzadeh.sahar.1999@gmail.com>
2024-06-22 10:47:06 +0330

    "New Files Added."

---
+++ GPU-initialize.py
@@ -0,0 +1,13 @@
+### Initialize GPU for mac
+#conda create --name 3D-Unet-brain-tumor-analysis python=3.9
+#conda activate 3D-Unet-brain-tumor-analysis
+# conda install -c apple tensorflow-deps
+# pip install tensorflow-macos
+# pip install tensorflow-metal
+
+import tensorflow as tf
+
+print("TensorFlow version:", tf.__version__)
+print("Is GPU available:", tf.config.list_physical_devices('GPU'))
+
+#Is GPU available: [PhysicalDevice(name='/physical_device:GPU:0', device_type='GPU')]
